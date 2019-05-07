6976ee216f48fd7c6f680c4fbf8d4ad8029bf950
Songyu <yusong1108@gmail.com>
2019-05-08 07:53:42 +0800

    122. Best Time to Buy and Sell Stock II

---
+++ LC-Swift/LC-Swift/Array/0122_BestTimeToBuyAndSellStockII.swift
@@ -0,0 +1,63 @@
+//
+//  0122_BestTimeToBuyAndSellStockII.swift
+//  LC-Swift
+//
+//  Created by Songyu on 2019/5/8.
+//  Copyright © 2019 Songyu. All rights reserved.
+//
+
+import Foundation
+
+/**
+ 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ 
+ 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+ 
+ 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ 
+ 示例 1:
+ 
+ 输入: [7,1,5,3,6,4]
+ 输出: 7
+ 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ 随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+ 
+ 示例 2:
+ 
+ 输入: [1,2,3,4,5]
+ 输出: 4
+ 解释: 在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ 注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。
+ 因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+ 
+ 示例 3:
+ 
+ 输入: [7,6,4,3,1]
+ 输出: 0
+ 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ */
+
+class BestTimeToBuyAndSellStockIISolution {
+    
+    // TimeComplexity: O(n) | SpaceComplexity: O(1)
+    // Thought: 相比 I 而言，这里可以无限买卖，仅需买入前卖出，即买入转移方程需要变化为 min(上次买入, 收益 - 本次买入)。
+    // 执行用时 : 68 ms, 在Best Time to Buy and Sell Stock II的Swift提交中击败了62.20% 的用户
+    // 内存消耗 : 19.2 MB, 在Best Time to Buy and Sell Stock II的Swift提交中击败了15.05% 的用户
+    private func maxProfit(_ prices: [Int]) -> Int {
+        guard prices.count > 1 else {
+            return 0
+        }
+        var profit = 0
+        var buy = -prices[0]
+        for price in prices {
+            buy = max(buy, profit - price)
+            profit = max(profit, price + buy)
+        }
+        return profit
+    }
+    
+    // Testcase: [7,1,5,3,6,4] -> 7 | [7,6,4,3,1] -> 0
+    func test() {
+        print(maxProfit([7,1,5,3,6,4]))
+    }
+}
